0038b9a1325715049e4e5ecaf6b9e248a85d47dd
Adam Cowley <adam@adamcowley.co.uk>
2023-03-30 10:06:39 +0100

    Add cypher variable to first challenge (#4)
    * Add cypher variable to first challenge

--- src/challenges/create-driver/solution.ts
+++ src/challenges/create-driver/solution.ts
@@ -15,27 +15,29 @@ const {
 
 async function main() {
   // tag::solution[]
-  // Create a Driver Instance
+  // Create a Driver Instance using neo4j.driver()
   const driver = neo4j.driver(
     NEO4J_URI,
     neo4j.auth.basic(NEO4J_USERNAME, NEO4J_PASSWORD)
   )
 
-  // Open a new Session
+  // Open a new Session using driver.session()
   const session = driver.session()
 
   try {
-    // Run a Cypher statement
-    const res = await session.run(`
+    // Run this Cypher statement using session.run()
+    const cypher = `
       MATCH (p:Person)-[:DIRECTED]->(:Movie {title: $title})
       RETURN p.name AS Director
-    `, { title: 'Toy Story' })
+    `
+    const params = { title: 'Toy Story' }
+
+    const res = await session.run(cypher, params)
 
     // Log the Director value of the first record
     console.log(res.records[0].get('Director'))
   }
   finally {
-    // Close the session
     await session.close()
   }
   // end::solution[]
